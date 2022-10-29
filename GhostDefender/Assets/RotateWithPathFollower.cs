383dd8f40eaa8d3ecc0cab21463fcff2d326d050
kmaxi <esst936@gmail.com>
2022-10-29 14:45:53 +0200

    Added rotation animation to the light

--- GhostDefender/Assets/RotateWithPathFollower.cs
+++ GhostDefender/Assets/RotateWithPathFollower.cs
@@ -8,6 +8,10 @@ public class RotateWithPathFollower : MonoBehaviour
     [SerializeField] private PathFollower pathFollower;
 
     private Vector2Int goingDirection = Vector2Int.zero;
+
+    Quaternion startRotation;
+    Quaternion endRotation;
+    float rotationProgress = -1;
     
     // Start is called before the first frame update
     void Start()
@@ -19,6 +23,15 @@ public class RotateWithPathFollower : MonoBehaviour
     void Update()
     {
         UpdateDirection();
+        
+        if (rotationProgress < 1 && rotationProgress >= 0){
+            rotationProgress += Time.deltaTime * 5;
+
+            // Assign the interpolated rotation to transform.rotation
+            // It will range from startRotation (rotationProgress == 0) to endRotation (rotationProgress >= 1)
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, rotationProgress);
+        }
+        
     }
 
     private void UpdateDirection()
@@ -30,7 +43,12 @@ public class RotateWithPathFollower : MonoBehaviour
         
         
         float rotZ = Mathf.Atan2(goingDirection.y, goingDirection.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotZ - 90);
+        endRotation = Quaternion.Euler(0f, 0f, rotZ - 90);
+
+        startRotation = transform.rotation;
+
+        rotationProgress = 0;
+        
         
     }
 }
