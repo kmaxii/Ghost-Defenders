bb05767af24839e863c82f51c004935498c43025
a19teosc <81693369+a19teosc@users.noreply.github.com>
2022-10-30 11:42:55 +0100

    Perfect screaming, 100%

--- GhostDefender/Assets/Scripts/Projectiles/ScreamGun.cs
+++ GhostDefender/Assets/Scripts/Projectiles/ScreamGun.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Scriptable_objects;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 public class ScreamGun : Gun
 {
     private float bulletSize;
-
+    [SerializeField] private AudioEvent audioEvent;
+    [SerializeField] private GameEventWithAudioEvent gEvent;
+    
     protected override void SettingUpBullet(Bullet bullet)
     {
+        gEvent.Raise(audioEvent);
         Assert.IsTrue(bullet is SoundWave);
         SoundWave soundWave = (SoundWave) bullet;
 
