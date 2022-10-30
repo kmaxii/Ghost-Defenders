ea0e4d5e7109e469619f642aea072f4a1235bc38
kmaxi <esst936@gmail.com>
2022-10-30 14:29:23 +0100

    Added max amount of same audio

--- GhostDefender/Assets/Scripts/EveryProject/AudioReceiver.cs
+++ GhostDefender/Assets/Scripts/EveryProject/AudioReceiver.cs
@@ -7,7 +7,7 @@ public class AudioReceiver : MonoBehaviour
 
     private readonly List<AudioSource> _audioSources = new List<AudioSource>();
 
-    private readonly Dictionary<AudioEvent, List<int>> _audioEventsPlaying = new Dictionary<AudioEvent, List<int>>();
+    private readonly Dictionary<AudioEvent, List<AudioSource>> _audioEventsPlaying = new Dictionary<AudioEvent, List<AudioSource>>();
 
 
     // Update is called once per frame
@@ -22,11 +22,30 @@ public class AudioReceiver : MonoBehaviour
                 _audioSources.RemoveAt(10);
             }
         }
+
+        foreach (var list in _audioEventsPlaying.Values)
+        {
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                AudioSource audio = list[i];
+             
+                if (audio.isPlaying)
+                    continue;
+                
+                list.RemoveAt(i);
+            }
+        }
     }
 
 
     public void OnReceiveAudio(AudioEvent audioEvent)
     {
+        if (!_audioEventsPlaying.ContainsKey(audioEvent))
+            _audioEventsPlaying.Add(audioEvent, new List<AudioSource>());
+        
+        if (_audioEventsPlaying[audioEvent].Count > 10)
+            return;
+        
         //Searches for a not playing audio source
         for (int i = 0; i < _audioSources.Count; i++)
         {
@@ -35,7 +54,7 @@ public class AudioReceiver : MonoBehaviour
                 continue;
             //Found unused audio source. Playing using that
             audioEvent.Play(audioSource);
-            AddToPlaying(audioEvent, i);
+            AddToPlaying(audioEvent, audioSource);
 
             return;
         }
@@ -45,10 +64,9 @@ public class AudioReceiver : MonoBehaviour
         audioEvent.Play(_audioSources[^1]);
     }
 
-    private void AddToPlaying(AudioEvent audioEvent, int audioSource)
+    private void AddToPlaying(AudioEvent audioEvent, AudioSource audioSource)
     {
-        if (!_audioEventsPlaying.ContainsKey(audioEvent))
-            _audioEventsPlaying.Add(audioEvent, new List<int>());
+     _audioEventsPlaying[audioEvent].Add(audioSource);
         
     }
 }
